79c258c34263548149ef0754f9a6f32ceb4e7aeb
Ahmed <tanvirahmed754575@gmail.com>
2026-03-05 23:09:55 +0600

    setup project init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Source Code Analysis RAG',
+    version= '1.0',
+    author= 'Tanvir Ahmed',
+    author_email= 'tanvirahmed754575@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
